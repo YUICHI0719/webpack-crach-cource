2c606d215c44eec150dc2d730597c5fc9521b303
Yuichi Kanno <yuichi.kanno@nijibox.co.jp>
2020-04-27 12:04:05 +0900

    eslint-loaderでJavaScriptの静的解析をリアルタイムに実行しよう

--- webpack.config.js
+++ webpack.config.js
@@ -5,63 +5,69 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 const outputPath = path.resolve(__dirname, 'dist');
-console.log({outputPath});
+console.log({ outputPath });
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: outputPath,
-    filename: 'main.js'
+    filename: 'main.js',
   },
   module: {
     rules: [
-      { 
-        test: /\.jsx?$/, 
-        exclude: /node_modules/, 
-        loader: "babel-loader"
+      {
+        enforce: 'pre',
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
       },
       {
         test: /\.(sc|c)ss$/,
         use: [MiniCssExtractPlugin.loader,
-              'css-loader',
-              'sass-loader']
+          'css-loader',
+          'sass-loader'],
       },
       {
         test: /\.(jpe?g|png|gif|svg|ico)$/i,
         loader: 'url-loader',
         options: {
           limit: 2048,
-          name: './images/[name].[ext]'
-        }
+          name: './images/[name].[ext]',
+        },
       },
       {
         test: /\.html$/,
-        loader: 'html-loader'
-      }
-    ]
+        loader: 'html-loader',
+      },
+    ],
   },
   devServer: {
-    contentBase: outputPath
+    contentBase: outputPath,
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: './src/index.html',
-      filename: './index.html'
+      filename: './index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].[hash].css'
-    })
+      filename: '[name].[hash].css',
+    }),
   ],
   optimization: {
     minimizer: [new UglifyJsPlugin({
-        uglifyOptions: {
-          compress: {
-            drop_console: true
-          }
-        }
-      }),
-      new OptimizeCSSAssetsPlugin({})
-    ]
+      uglifyOptions: {
+        compress: {
+          drop_console: true,
+        },
+      },
+    }),
+    new OptimizeCSSAssetsPlugin({}),
+    ],
   },
-  devtool: 'eval-source-map'
-}
+  devtool: 'eval-source-map',
+};
